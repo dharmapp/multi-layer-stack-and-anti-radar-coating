1f3e16fa4023a1d3823852a6ed18ca612c985d98
dharmapp <145416733+dharmapp@users.noreply.github.com>
2025-06-26 10:29:23 +0300

    Update matrix_T.m

--- matrix_T.m
+++ matrix_T.m
@@ -1,3 +1,16 @@
+%This function calculate the matrix that describes transition between
+%adjacent medium interfaces
+%Input  : 
+%   betaj       = propagation constant in medium j
+%   betapj1     = propagation constant in medium j+1
+%   pj          = polarization factor in medium j
+%   pjp1        = polarization factor in medium j+1
+%   thickness   = thickness of medium j
+%   all the inputs are in the SI units
+
+%Output :
+%   A 2x2 matrix with elements that describes a propagation of E fields
+%   inside a medium
 function [matT] = matrix_T(betaj,betajp1,pj,pjp1);
 sj = (pj.*betaj + pjp1.*betajp1)./(2.*pj.*betaj);
 dj = (pj.*betaj - pjp1.*betajp1)./(2.*pj.*betaj);
